bf1ca0fb7bd3a6af22126edf9100ab9d2ebf654e
Marcus Jeong <marcus.jeong96@gmail.com>
2023-01-20 21:38:04 -0500

    started animation work on the home page

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import '../backgroundHome.css';
 
 const Home = () => {
-    return <div>Home Page</div>
+    return (
+        <>
+            <div className="background">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div>Home Page</div>
+        </>
+    )
+    
 }
 
 export default Home;
